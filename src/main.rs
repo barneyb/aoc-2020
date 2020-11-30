0a1effefcc5025094e696a0e1558b630d0dbdc79
Barney Boisvert <bboisvert@gmail.com>
2020-11-29 19:37:07 -0800

    part one

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{:?}",
+        read_input()
+            .chars()
+            .map(|c| match c {
+                '(' => 1,
+                ')' => -1,
+                _ => panic!("Unrecognized char '{}'", c),
+            })
+            .sum::<i32>()
+    );
+}
+
+fn read_input() -> String {
+    fs::read_to_string("input.txt").unwrap().trim().to_string()
 }
