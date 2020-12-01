184a9fcfb3a29e366900b1a83c7bdab88cd15675
Barney Boisvert <bboisvert@gmail.com>
2020-11-30 21:15:36 -0800

    part two

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ fn main() {
         .collect::<Vec<i32>>();
     'outer: for a in &expenses {
         for b in &expenses {
-            if a + b == 2020 {
-                println!("{} * {} = {}", a, b, a * b);
-                break 'outer;
+            for c in &expenses {
+                if a + b + c == 2020 {
+                    println!("{} * {} * {} = {}", a, b, c, a * b * c);
+                    break 'outer;
+                }
             }
         }
     }
