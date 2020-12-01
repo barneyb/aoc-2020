9df5533a5faf6e627162877ea7b5b372427b097c
Barney Boisvert <bboisvert@gmail.com>
2020-11-30 21:14:17 -0800

    part one

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
 use aoc_2020::read_input;
 
 fn main() {
-    println!(
-        "{:?}",
-        read_input()
-            .chars()
-            .map(|c| match c {
-                '(' => 1,
-                ')' => -1,
-                _ => panic!("Unrecognized char '{}'", c),
-            })
-            .enumerate()
-            .fold((0, None as Option<usize>), |(mut floor, pos), (i, d)| {
-                if pos.is_none() {
-                    floor += d;
-                    if floor == -1 {
-                        return (floor, Some(i + 1));
-                    }
-                }
-                (floor, pos)
-            })
-            .1
-            .unwrap()
-    );
+    let expenses = read_input()
+        .lines()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<i32>>();
+    'outer: for a in &expenses {
+        for b in &expenses {
+            if a + b == 2020 {
+                println!("{} * {} = {}", a, b, a * b);
+                break 'outer;
+            }
+        }
+    }
 }
