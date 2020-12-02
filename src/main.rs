318c16f752a4dd5738d9b9dea498e3825c645d96
Barney Boisvert <bboisvert@gmail.com>
2020-12-01 19:03:05 -0800

    deref once is faster

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use aoc_2020::read_lines;
 
 fn main() {
     let expenses = read_lines(|s| s.parse::<i32>().unwrap());
-    'outer: for (i, a) in expenses.iter().enumerate() {
-        for (j, b) in expenses.iter().skip(i + 1).enumerate() {
-            for c in expenses.iter().skip(j + 1) {
+    'outer: for (i, &a) in expenses.iter().enumerate() {
+        for (j, &b) in expenses.iter().skip(i + 1).enumerate() {
+            for &c in expenses.iter().skip(j + 1) {
                 if a + b + c == 2020 {
                     println!("{} * {} * {} = {}", a, b, c, a * b * c);
                     break 'outer;
