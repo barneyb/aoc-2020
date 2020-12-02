0afedd648a2a5c08f25a02b3220f6936c5068f8e
Barney Boisvert <bboisvert@gmail.com>
2020-12-01 21:19:20 -0800

    part one

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
 use aoc_2020 as aoc;
 
+#[derive(Debug)]
+struct Policy {
+    min: usize,
+    max: usize,
+    char: char,
+}
+
+#[derive(Debug)]
+struct Record {
+    policy: Policy,
+    password: String,
+}
+
 fn main() {
-    let expenses = aoc::read_lines(|s| s.parse::<i32>().unwrap());
-    'outer: for (i, &a) in expenses.iter().enumerate() {
-        for (j, &b) in expenses.iter().skip(i + 1).enumerate() {
-            let remaining = 2020 - a - b;
-            if remaining <= 0 {
-                continue;
-            }
-            for &c in expenses.iter().skip(j + 1) {
-                if c == remaining {
-                    println!("{} * {} * {} = {}", a, b, c, a * b * c);
-                    break 'outer;
-                }
-            }
+    let database = aoc::read_lines(|s| {
+        let di = s.find('-').expect("failed to find dash");
+        let si = s.find(' ').expect("failed to find space");
+        let ci = s.find(':').expect("failed to find colon");
+        let min = s[0..di].parse().expect("failed to parse min");
+        let max = s[(di + 1)..si].parse().expect("failed to parse max");
+        let &char = &s[(si + 1)..ci].chars().next().expect("failed to get policy char");
+        let password = String::from(s[(ci + 1)..s.len()].trim());
+        Record {
+            policy: Policy {
+                min,
+                max,
+                char,
+            },
+            password,
         }
-    }
+    });
+    println!("{}", database.iter().filter(|r| {
+        let c = r.password.chars().filter(|&c| c == r.policy.char).count();
+        c >= r.policy.min && c <= r.policy.max
+    }).count())
 }
