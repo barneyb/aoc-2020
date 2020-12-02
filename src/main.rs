37478890a7fbd2b601bfa44c0c23c7b30bb2f63f
Barney Boisvert <bboisvert@gmail.com>
2020-12-01 19:27:47 -0800

    reduce the amount of arithmetic

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use aoc_2020::read_lines;
+use aoc_2020 as aoc;
 
 fn main() {
-    let expenses = read_lines(|s| s.parse::<i32>().unwrap());
+    let expenses = aoc::read_lines(|s| s.parse::<i32>().unwrap());
     'outer: for (i, &a) in expenses.iter().enumerate() {
         for (j, &b) in expenses.iter().skip(i + 1).enumerate() {
-            if a + b > 2020 {
+            let remaining = 2020 - a - b;
+            if remaining <= 0 {
                 continue;
             }
             for &c in expenses.iter().skip(j + 1) {
-                if a + b + c == 2020 {
+                if c == remaining {
                     println!("{} * {} * {} = {}", a, b, c, a * b * c);
                     break 'outer;
                 }
