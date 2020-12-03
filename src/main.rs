56dd3e7b680eeda5abe92f65132bfbf7837eab7f
Barney Boisvert <bboisvert@gmail.com>
2020-12-02 18:50:08 -0800

    implement FromStr for Record, and add is_valid for Policy

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+#[macro_use]
+extern crate lazy_static;
+
 use aoc_2020 as aoc;
 use regex::Regex;
+use std::str::FromStr;
 
 #[derive(Debug)]
 struct Policy {
@@ -8,30 +12,49 @@ struct Policy {
     char: char,
 }
 
+impl Policy {
+    fn is_valid(&self, pw: &str) -> bool {
+        let first = self.test_char(pw, self.min - 1);
+        let last = self.test_char(pw, self.max - 1);
+        first ^ last
+    }
+
+    fn test_char(&self, pw: &str, i: usize) -> bool {
+        pw.len() > i && pw.chars().nth(i).expect("failed to get char") == self.char
+    }
+}
+
 #[derive(Debug)]
 struct Record {
     policy: Policy,
     password: String,
 }
 
-fn main() {
-    let re = Regex::new(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$").unwrap();
-    let database = aoc::read_lines(|s| {
-        let parts = re.captures(s).unwrap();
-        Record {
+lazy_static! {
+    static ref RECORD_RE: Regex = Regex::new(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$").unwrap();
+}
+
+impl FromStr for Record {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = RECORD_RE.captures(s).unwrap();
+        Ok(Record {
             policy: Policy {
                 min: parts.get(1).unwrap().as_str().parse().unwrap(),
                 max: parts.get(2).unwrap().as_str().parse().unwrap(),
                 char: parts.get(3).unwrap().as_str().chars().next().unwrap(),
             },
             password: String::from(parts.get(4).unwrap().as_str()),
-        }
-    });
-    println!("{}", database.iter().filter(|r| {
-        let min = r.policy.min - 1;
-        let first = r.password.len() > min && r.password.chars().nth(min).expect("failed to get first char") == r.policy.char;
-        let max = r.policy.max - 1;
-        let last = r.password.len() > max && r.password.chars().nth(max).expect("failed to get second char") == r.policy.char;
-        (first && !last) || (!first && last)
-    }).count())
+        })
+    }
+}
+
+fn main() {
+    let database = aoc::read_lines(|s| s.parse::<Record>().unwrap());
+    let num_valid = database
+        .iter()
+        .filter(|r| r.policy.is_valid(&r.password))
+        .count();
+    println!("{}", num_valid)
 }
