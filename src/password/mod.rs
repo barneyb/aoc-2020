8f3fb51034e24b05eeaf1a7fc871f3ac3ed656c8
Barney Boisvert <bboisvert@gmail.com>
2020-12-05 11:02:59 -0800

    implement FromStr for Policy, so Record doesn't have to do it all

--- src/password/mod.rs
+++ src/password/mod.rs
@@ -19,6 +19,23 @@ impl Policy {
     }
 }
 
+impl FromStr for Policy {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let di = s.find('-').expect("failed to find dash");
+        let si = s.find(' ').expect("failed to find space");
+        Ok(Policy {
+            min: s[0..di].parse().expect("failed to parse min"),
+            max: s[(di + 1)..si].parse().expect("failed to parse max"),
+            char: s[(si + 1)..s.len()]
+                .chars()
+                .next()
+                .expect("failed to get policy char"),
+        })
+    }
+}
+
 #[derive(Debug)]
 pub struct Record {
     policy: Policy,
@@ -35,19 +52,33 @@ impl FromStr for Record {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let di = s.find('-').expect("failed to find dash");
-        let si = s.find(' ').expect("failed to find space");
         let ci = s.find(':').expect("failed to find colon");
         Ok(Record {
-            policy: Policy {
-                min: s[0..di].parse().expect("failed to parse min"),
-                max: s[(di + 1)..si].parse().expect("failed to parse max"),
-                char: s[(si + 1)..ci]
-                    .chars()
-                    .next()
-                    .expect("failed to get policy char"),
-            },
+            policy: s[0..ci].parse().expect("failed to parse policy"),
             password: String::from(s[(ci + 1)..s.len()].trim()),
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_policy_parse() {
+        let p = "3-4 q".parse::<Policy>().unwrap();
+        assert_eq!(p.min, 3);
+        assert_eq!(p.max, 4);
+        assert_eq!(p.char, 'q');
+    }
+
+    #[test]
+    fn test_record_parse() {
+        let r = "3-4 q: qqqn".parse::<Record>().unwrap();
+        assert_eq!(r.password, "qqqn");
+        let p = r.policy;
+        assert_eq!(p.min, 3);
+        assert_eq!(p.max, 4);
+        assert_eq!(p.char, 'q');
     }
 }
