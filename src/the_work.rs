1475a25cd8f54a128831aba7cf0c314c6590d844
Barney Boisvert <bboisvert@gmail.com>
2020-12-08 21:02:25 -0800

    part two

--- src/the_work.rs
+++ src/the_work.rs
@@ -4,6 +4,36 @@ use std::str::FromStr;
 pub fn the_work() {
     let instructions = aoc::read_lines(|l| l.parse::<Ins>().unwrap());
     println!("{:?}", evaluate(&instructions));
+    println!("{:?}", munge_until_exits(&instructions));
+}
+
+fn munge_until_exits(instructions: &Vec<Ins>) -> i32 {
+    let mut scratch = Vec::with_capacity(instructions.len());
+    'to_flip: for flip in 0..instructions.len() {
+        scratch.clear();
+        for i in 0..instructions.len() {
+            if flip == i {
+                scratch.push(match instructions[i].code {
+                    Nop => Ins {
+                        code: Jmp,
+                        param: instructions[i].param,
+                    },
+                    Acc => continue 'to_flip,
+                    Jmp => Ins {
+                        code: Nop,
+                        param: instructions[i].param,
+                    },
+                });
+            } else {
+                scratch.push(instructions[i]);
+            }
+        }
+        match evaluate(&scratch) {
+            Res::Loop(_) => {}
+            Res::Exit(r) => return r,
+        }
+    }
+    panic!("didn't find an exiting munge?!")
 }
 
 #[derive(Eq, PartialEq, Debug)]
@@ -39,6 +69,7 @@ fn evaluate(instructions: &Vec<Ins>) -> Res<i32> {
     }
 }
 
+#[derive(Copy, Clone)]
 enum OpCode {
     Nop,
     Acc,
@@ -59,6 +90,7 @@ impl FromStr for OpCode {
     }
 }
 
+#[derive(Copy, Clone)]
 struct Ins {
     code: OpCode,
     param: i32,
@@ -120,5 +152,15 @@ acc +6";
             .map(|l| l.parse().unwrap())
             .collect::<Vec<Ins>>();
         assert_eq!(Res::Exit(8), evaluate(&instructions));
+    }
+
+    #[test]
+    fn example_two() {
+        let instructions = EXAMPLE_INPUT
+            .trim()
+            .lines()
+            .map(|l| l.parse().unwrap())
+            .collect::<Vec<Ins>>();
+        assert_eq!(8, munge_until_exits(&instructions));
     }
 }
