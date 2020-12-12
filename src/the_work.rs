a2563d85122c4089e72a6a7ac78eaef15cd00be6
Barney Boisvert <bboisvert@gmail.com>
2020-12-12 07:28:05 -0800

    part one

--- src/the_work.rs
+++ src/the_work.rs
@@ -1,77 +1,263 @@
-use aoc_2020::histogram::Histogram;
-use aoc_2020::read_lines;
+use aoc_2020::read_input;
+use std::fmt;
+use std::fmt::{Display, Formatter, Write};
+use std::str::FromStr;
+use Loc::*;
 
 pub fn the_work() {
-    let adapters = read_lines(|l| l.parse::<usize>().unwrap());
-    let diffs = compute_diffs(&adapters);
-    println!("{}", jolt_distribution(&diffs));
-    println!("{}", count_arrangements(&diffs));
+    let map = load_map(&read_input());
+    let stable = stabilize_map(&map);
+    println!("{}", stable.occupied_seat_count());
 }
 
-fn jolt_distribution(diffs: &[usize]) -> usize {
-    let mut hist = Vec::new();
-    for &d in diffs {
-        hist.increment_bucket(d);
-    }
-    hist[1] * hist[3]
-}
-
-fn compute_diffs(adapters: &[usize]) -> Vec<usize> {
-    let mut scratch = Vec::from(adapters);
-    scratch.sort();
-    scratch.push(scratch[scratch.len() - 1] + 3);
-    let mut diffs = Vec::new();
-    let mut last = 0;
-    for jolts in scratch {
-        diffs.push(jolts - last);
-        last = jolts;
-    }
-    diffs
-}
-
-fn count_arrangements(diffs: &[usize]) -> usize {
-    let mut product = 1;
-    let mut len = 0;
-    for &d in diffs {
-        if d == 1 {
-            len += 1;
-        } else if len > 0 {
-            product *= match len {
-                1 => 1,
-                2 => 2,
-                3 => 4,
-                4 => 7,
-                _ => panic!("Runs of {} aren't supported", len),
-            };
-            len = 0;
+fn load_map(s: &str) -> Map {
+    let lines: Vec<&str> = s.trim().split('\n').collect();
+    let width = lines[0].len();
+    let height = lines.len();
+    let mut locations = Vec::with_capacity(width * height);
+    for l in lines {
+        for c in l.chars() {
+            locations.push(c.to_string().parse().unwrap());
         }
     }
-    product
+    Map::new(width, height, locations)
+}
+
+#[derive(Eq, PartialEq, Copy, Clone, Debug)]
+enum Loc {
+    Floor,
+    Empty,
+    Occupied,
+}
+
+impl FromStr for Loc {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "." => Ok(Floor),
+            "L" => Ok(Empty),
+            "#" => Ok(Occupied),
+            _ => Err(format!("Unrecognized '{}' in map", s)),
+        }
+    }
+}
+
+impl Display for Loc {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.write_char(match self {
+            Floor => '.',
+            Empty => 'L',
+            Occupied => '#',
+        })
+    }
+}
+
+#[derive(Eq, PartialEq)]
+struct Map {
+    width: usize,
+    height: usize,
+    locations: Vec<Loc>,
+}
+
+impl Map {
+    fn new(width: usize, height: usize, locations: Vec<Loc>) -> Map {
+        Map {
+            width,
+            height,
+            locations,
+        }
+    }
+
+    fn occupied_neighbor_count(&self, i: usize) -> usize {
+        let x = i % self.width;
+        let y = i / self.width;
+        let mut pairs = Vec::new();
+        // the column to the left
+        if x > 0 {
+            if y > 0 {
+                pairs.push((x - 1, y - 1));
+            }
+            pairs.push((x - 1, y));
+            if y < self.height - 1 {
+                pairs.push((x - 1, y + 1));
+            }
+        }
+        // this column
+        if y > 0 {
+            pairs.push((x, y - 1));
+        }
+        if y < self.height - 1 {
+            pairs.push((x, y + 1));
+        }
+        // the column to the right
+        if x < self.width - 1 {
+            if y > 0 {
+                pairs.push((x + 1, y - 1));
+            }
+            pairs.push((x + 1, y));
+            if y < self.height - 1 {
+                pairs.push((x + 1, y + 1));
+            }
+        }
+        pairs
+            .iter()
+            .filter(|(x, y)| {
+                debug_assert!(y * self.width + x < self.width * self.height);
+                self.locations[y * self.width + x] == Occupied
+            })
+            .count()
+    }
+
+    fn step(&self) -> Map {
+        let locations = self
+            .locations
+            .iter()
+            .enumerate()
+            .map(|(i, l)| match l {
+                Floor => Floor,
+                Empty => match self.occupied_neighbor_count(i) {
+                    0 => Occupied,
+                    _ => Empty,
+                },
+                Occupied => {
+                    if self.occupied_neighbor_count(i) >= 4 {
+                        Empty
+                    } else {
+                        Occupied
+                    }
+                }
+            })
+            .collect::<Vec<Loc>>();
+        debug_assert_eq!(locations.len(), self.locations.len());
+        Map {
+            width: self.width,
+            height: self.height,
+            locations,
+        }
+    }
+
+    #[cfg(test)]
+    fn empty_seat_count(&self) -> usize {
+        self.locations.iter().filter(|&&it| it == Empty).count()
+    }
+
+    fn occupied_seat_count(&self) -> usize {
+        self.locations.iter().filter(|&&it| it == Occupied).count()
+    }
+}
+
+impl Display for Map {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut s = String::with_capacity(self.width * (self.height + 1));
+        for y in 0..self.height {
+            if y > 0 {
+                s.push('\n');
+            }
+            for x in 0..self.width {
+                s.push_str(&self.locations[y * self.width + x].to_string());
+            }
+        }
+        write!(f, "{}", s)
+    }
+}
+
+fn stabilize_map(m: &Map) -> Map {
+    let mut curr = m.step();
+    loop {
+        let next = curr.step();
+        if curr == next {
+            return next;
+        }
+        curr = next;
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const EXAMPLE_ONE: [usize; 11] = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4];
-    const EXAMPLE_TWO: [usize; 31] = [
-        28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8,
-        17, 7, 9, 4, 2, 34, 10, 3,
-    ];
+    const EXAMPLE_ONE: &str = "
+L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL";
 
     #[test]
-    fn test_jolt_distribution() {
-        let diffs = compute_diffs(&EXAMPLE_ONE);
-        assert_eq!(35, jolt_distribution(&diffs));
-        let diffs = compute_diffs(&EXAMPLE_TWO);
-        assert_eq!(220, jolt_distribution(&diffs));
+    fn test_load_map() {
+        let m = load_map(EXAMPLE_ONE);
+        assert_eq!(10, m.width);
+        assert_eq!(10, m.height);
+        assert_eq!(71, m.empty_seat_count());
+        assert_eq!(0, m.occupied_seat_count());
     }
 
     #[test]
-    fn test_arrangements() {
-        let diffs = compute_diffs(&EXAMPLE_ONE);
-        assert_eq!(8, count_arrangements(&diffs));
-        let diffs = compute_diffs(&EXAMPLE_TWO);
-        assert_eq!(19208, count_arrangements(&diffs));
+    fn test_display() {
+        assert_eq!(EXAMPLE_ONE.trim(), load_map(EXAMPLE_ONE).to_string())
+    }
+
+    #[test]
+    fn test_step() {
+        let m = load_map(EXAMPLE_ONE);
+        assert_eq!(0, m.occupied_seat_count());
+        let m = m.step();
+        assert_eq!(
+            "#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##",
+            m.to_string()
+        );
+        assert_eq!(71, m.occupied_seat_count());
+        let m = m.step();
+        assert_eq!(
+            "#.LL.L#.##
+#LLLLLL.L#
+L.L.L..L..
+#LLL.LL.L#
+#.LL.LL.LL
+#.LLLL#.##
+..L.L.....
+#LLLLLLLL#
+#.LLLLLL.L
+#.#LLLL.##",
+            m.to_string()
+        );
+        assert_eq!(20, m.occupied_seat_count());
+        let m = m.step();
+        assert_eq!(
+            "#.##.L#.##
+#L###LL.L#
+L.#.#..#..
+#L##.##.L#
+#.##.LL.LL
+#.###L#.##
+..#.#.....
+#L######L#
+#.LL###L.L
+#.#L###.##",
+            m.to_string()
+        );
+        assert_eq!(51, m.occupied_seat_count());
+    }
+
+    #[test]
+    fn test_stabilize() {
+        let m = load_map(EXAMPLE_ONE);
+        let m = stabilize_map(&m);
+        assert_eq!(37, m.occupied_seat_count());
     }
 }
