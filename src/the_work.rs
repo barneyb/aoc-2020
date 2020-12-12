f4ea2d9df2e10c62af17008b42a0861d80d7f24a
Barney Boisvert <bboisvert@gmail.com>
2020-12-12 08:46:27 -0800

    part two

--- src/the_work.rs
+++ src/the_work.rs
@@ -70,43 +70,144 @@ impl Map {
     }
 
     fn occupied_neighbor_count(&self, i: usize) -> usize {
+        let mut count = 0;
+        let mut x = i % self.width;
+        let mut y = i / self.width;
+        loop {
+            if x == 0 || y == 0 {
+                break;
+            }
+            x -= 1;
+            y -= 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
+            }
+        }
         let x = i % self.width;
+        let mut y = i / self.width;
+        loop {
+            if y == 0 {
+                break;
+            }
+            y -= 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        let mut x = i % self.width;
+        let mut y = i / self.width;
+        loop {
+            if x == self.width - 1 || y == 0 {
+                break;
+            }
+            x += 1;
+            y -= 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        let mut x = i % self.width;
         let y = i / self.width;
-        let mut pairs = Vec::new();
-        // the column to the left
-        if x > 0 {
-            if y > 0 {
-                pairs.push((x - 1, y - 1));
+        loop {
+            if x == self.width - 1 {
+                break;
             }
-            pairs.push((x - 1, y));
-            if y < self.height - 1 {
-                pairs.push((x - 1, y + 1));
-            }
-        }
-        // this column
-        if y > 0 {
-            pairs.push((x, y - 1));
-        }
-        if y < self.height - 1 {
-            pairs.push((x, y + 1));
-        }
-        // the column to the right
-        if x < self.width - 1 {
-            if y > 0 {
-                pairs.push((x + 1, y - 1));
-            }
-            pairs.push((x + 1, y));
-            if y < self.height - 1 {
-                pairs.push((x + 1, y + 1));
+            x += 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
             }
         }
-        pairs
-            .iter()
-            .filter(|(x, y)| {
-                debug_assert!(y * self.width + x < self.width * self.height);
-                self.locations[y * self.width + x] == Occupied
-            })
-            .count()
+        let mut x = i % self.width;
+        let mut y = i / self.width;
+        loop {
+            if x == self.width - 1 || y == self.height - 1 {
+                break;
+            }
+            x += 1;
+            y += 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        let x = i % self.width;
+        let mut y = i / self.width;
+        loop {
+            if y == self.height - 1 {
+                break;
+            }
+            y += 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        let mut x = i % self.width;
+        let mut y = i / self.width;
+        loop {
+            if x == 0 || y == self.height - 1 {
+                break;
+            }
+            x -= 1;
+            y += 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        let mut x = i % self.width;
+        let y = i / self.width;
+        loop {
+            if x == 0 {
+                break;
+            }
+            x -= 1;
+            match self.at(x, y) {
+                Floor => {}
+                Empty => break,
+                Occupied => {
+                    count += 1;
+                    break;
+                }
+            }
+        }
+        count
+    }
+
+    fn at(&self, x: usize, y: usize) -> &Loc {
+        &self.locations[y * self.width + x]
     }
 
     fn step(&self) -> Map {
@@ -121,7 +222,7 @@ impl Map {
                     _ => Empty,
                 },
                 Occupied => {
-                    if self.occupied_neighbor_count(i) >= 4 {
+                    if self.occupied_neighbor_count(i) >= 5 {
                         Empty
                     } else {
                         Occupied
@@ -224,40 +325,84 @@ L.LLLLL.LL";
         assert_eq!(71, m.occupied_seat_count());
         let m = m.step();
         assert_eq!(
-            "#.LL.L#.##
-#LLLLLL.L#
+            "#.LL.LL.L#
+#LLLLLL.LL
 L.L.L..L..
-#LLL.LL.L#
-#.LL.LL.LL
-#.LLLL#.##
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
 ..L.L.....
-#LLLLLLLL#
+LLLLLLLLL#
 #.LLLLLL.L
-#.#LLLL.##",
+#.LLLLL.L#",
             m.to_string()
         );
-        assert_eq!(20, m.occupied_seat_count());
+        assert_eq!(7, m.occupied_seat_count());
         let m = m.step();
         assert_eq!(
-            "#.##.L#.##
-#L###LL.L#
+            "#.L#.##.L#
+#L#####.LL
 L.#.#..#..
-#L##.##.L#
-#.##.LL.LL
-#.###L#.##
+##L#.##.##
+#.##.#L.##
+#.#####.#L
 ..#.#.....
-#L######L#
-#.LL###L.L
-#.#L###.##",
+LLL####LL#
+#.L#####.L
+#.L####.L#",
             m.to_string()
         );
-        assert_eq!(51, m.occupied_seat_count());
+        assert_eq!(53, m.occupied_seat_count());
     }
 
     #[test]
     fn test_stabilize() {
         let m = load_map(EXAMPLE_ONE);
         let m = stabilize_map(&m);
-        assert_eq!(37, m.occupied_seat_count());
+        assert_eq!(26, m.occupied_seat_count());
+    }
+
+    #[test]
+    fn example_two_1() {
+        let m = load_map(
+            "
+.......#.
+...#.....
+.#.......
+.........
+..#L....#
+....#....
+.........
+#........
+...#.....",
+        );
+        assert_eq!(8, m.occupied_neighbor_count(4 * 9 + 3));
+    }
+
+    #[test]
+    fn example_two_2() {
+        let m = load_map(
+            "
+.............
+.L.L.#.#.#.#.
+.............",
+        );
+        assert_eq!(0, m.occupied_neighbor_count(1 * 13 + 1));
+        assert_eq!(1, m.occupied_neighbor_count(1 * 13 + 3));
+    }
+
+    #[test]
+    fn example_two_3() {
+        let m = load_map(
+            "
+.##.##.
+#.#.#.#
+##...##
+...L...
+##...##
+#.#.#.#
+.##.##.",
+        );
+        assert_eq!(0, m.occupied_neighbor_count(3 * 7 + 3));
     }
 }
