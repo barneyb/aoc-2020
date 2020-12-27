23ab66159522975e397484fb6b8881a35f493118
Barney Boisvert <bboisvert@gmail.com>
2020-12-27 12:22:40 -0800

    assemble the first row of the grid

--- src/the_work.rs
+++ src/the_work.rs
@@ -1,70 +1,124 @@
 use aoc_2020::read_input;
-use petgraph::graph::Graph;
+use petgraph::graph::DiGraph;
+use petgraph::prelude::NodeIndex;
+use petgraph::visit::EdgeRef;
+use petgraph::Direction;
 use std::collections::HashMap;
 use std::fmt::{self, Display, Formatter};
+use std::rc::Rc;
 use std::str::FromStr;
 
 pub fn the_work() {
-    let input = read_input();
+    let tiles = parse(&read_input());
     // 1699 3229 1433 2351 ***pq
-    println!("{:?}", part_one(&input));
+    println!("{:?}", part_one(&tiles));
 }
 
+const NORTH: usize = 0;
+const EAST: usize = 1;
+const SOUTH: usize = 2;
+const WEST: usize = 3;
+
+#[derive(Debug, Clone)]
 struct Tile {
     num: usize,
     pixels: String,
-    north: String,
-    east: String,
-    south: String,
-    west: String,
-    f_north: String,
-    f_east: String,
-    f_south: String,
-    f_west: String,
+    dim: usize,
+    borders: Vec<String>,
 }
 
-fn usize_sqrt(n: usize) -> usize {
-    let r: usize = (n as f64).sqrt() as usize;
+fn sqrtusize(n: usize) -> usize {
+    let r = (n as f64).sqrt() as usize;
     debug_assert_eq!(r * r, n); // no floating point error!
     r
 }
 
 impl Tile {
-    fn new(num: usize, pixels: String) -> Tile {
-        let dim = usize_sqrt(pixels.len());
+    fn new(num: usize, pixels: &str) -> Tile {
+        let pixels = String::from(pixels);
+        let dim = sqrtusize(pixels.len());
+        Tile {
+            num,
+            dim,
+            borders: Tile::extract_borders(&pixels, dim),
+            pixels,
+        }
+    }
+
+    fn extract_borders(pixels: &String, dim: usize) -> Vec<String> {
         let north = String::from(&pixels[0..dim]);
         let east: String = pixels.chars().skip(dim - 1).step_by(dim).collect();
         let f_south = String::from(&pixels[(pixels.len() - dim)..]);
         let f_west: String = pixels.chars().step_by(dim).collect();
         let f_north = north.chars().rev().collect();
         let f_east = east.chars().rev().collect();
-        let south = f_south.chars().rev().collect();
-        let west = f_west.chars().rev().collect();
-        Tile {
-            num,
-            pixels,
+        vec![
             north,
             east,
-            south,
-            west,
+            f_south.chars().rev().collect(),
+            f_west.chars().rev().collect(),
             f_north,
             f_east,
             f_south,
             f_west,
-        }
+        ]
     }
 
-    fn edges(&self) -> Vec<&str> {
-        vec![
-            &self.north,
-            &self.east,
-            &self.south,
-            &self.west,
-            &self.f_north,
-            &self.f_east,
-            &self.f_south,
-            &self.f_west,
-        ]
+    fn transform<F>(&mut self, src: F)
+    where
+        F: Fn(usize, usize) -> (usize, usize),
+    {
+        let mut next = String::with_capacity(self.pixels.len());
+        let bytes = self.pixels.bytes().collect::<Vec<_>>();
+        for y in 0..self.dim {
+            for x in 0..self.dim {
+                let (a, b) = src(x, y);
+                next.push(bytes[a * self.dim + b] as char)
+            }
+        }
+        self.pixels = next;
+    }
+
+    /// I flip the tile along a vertical axis as if it were sitting on a table and you were to grab
+    /// the bottom edge, pick it up, roll your wrist over, and set it back down.
+    fn flip(&mut self) {
+        let dim = self.dim;
+        self.transform(|x, y| (y, dim - x - 1));
+        self.borders = Tile::extract_borders(&self.pixels, self.dim);
+    }
+
+    // I rotate the tile 90 degrees clockwise without picking it up.
+    fn rotate(&mut self, times: usize) {
+        let dim = self.dim;
+        for _ in 0..times {
+            // This is stupid and inefficient. But not wrong!
+            self.transform(|x, y| (dim - x - 1, y));
+        }
+        self.borders = Tile::extract_borders(&self.pixels, self.dim);
+    }
+
+    fn get_border(&self, dir: usize) -> &String {
+        &self.borders[dir]
+    }
+
+    fn get_flipped_border(&self, dir: usize) -> &String {
+        &self.borders[dir + 4]
+    }
+
+    // fn face_up_borders(&self) -> &[String] {
+    //     &self.borders[..4]
+    // }
+    //
+    // fn face_down_borders(&self) -> &[String] {
+    //     &self.borders[4..]
+    // }
+
+    fn all_borders(&self) -> &[String] {
+        &self.borders
+    }
+
+    fn which_border(&self, border: &String) -> usize {
+        self.borders.iter().position(|e| e == border).unwrap()
     }
 }
 
@@ -75,7 +129,7 @@ impl FromStr for Tile {
         let s = s.trim();
         let ci = s.find(':').expect("no colon?!");
         let num = s[5..ci].parse().unwrap();
-        Ok(Tile::new(num, s[(ci + 2)..].replace('\n', "")))
+        Ok(Tile::new(num, &s[(ci + 2)..].replace('\n', "")))
     }
 }
 
@@ -97,28 +151,142 @@ fn parse(input: &str) -> Vec<Tile> {
         .collect()
 }
 
-fn part_one(input: &str) -> usize {
-    let tiles = &parse(input);
-    let mut node_by_num = HashMap::new();
-    let mut node_by_edge = HashMap::new();
-    let mut graph = Graph::new();
-    for t in tiles {
-        let node = graph.add_node(&t.num);
-        if let Some(_) = node_by_num.insert(t.num, node) {
-            panic!("duplicate tile '{}'?!", t.num)
+fn part_one(tiles: &[Tile]) -> usize {
+    let graph = build_graph(tiles);
+    find_corners(&graph)
+        .iter()
+        .map(|&ni| graph.node_weight(ni).unwrap().num)
+        .product()
+}
+
+fn part_two(tiles: &[Tile]) -> usize {
+    let graph = build_graph(tiles);
+    let grid = assemble_grid(&graph);
+
+    println!("{:?}", grid.iter().map(|t| t.num).collect::<Vec<_>>());
+    tiles.len()
+}
+
+fn assemble_grid(graph: &DiGraph<&Tile, (usize, &String)>) -> Vec<Tile> {
+    let dim = sqrtusize(graph.node_count());
+    // first, find the top-left corner as an index:tile pair
+    let mut curr = Rc::new(top_left_corner(&graph));
+    let mut grid = Vec::with_capacity(graph.node_count());
+    grid.push(curr.clone());
+
+    // for each slot in the top row...
+    for _ in 1..dim {
+        // For each edge leaving curr (the node to the left), check and see if it's
+        // the edge for curr's EAST border (or the flip of it), and get the node at
+        // the other end. That'll be the next node in the row. We can't use the
+        // border directions in the graph edge directly, as the curr node's tile may
+        // have been flipped or rotated since the edges were wired up.
+        let ni = graph
+            .edges_directed(curr.0, Direction::Outgoing)
+            .find(|er| {
+                er.weight().1 == curr.1.get_border(EAST)
+                    || er.weight().1 == curr.1.get_flipped_border(EAST)
+            })
+            .unwrap()
+            .target();
+        // create a mungible Tile to throw in the grid
+        let mut tile = mungible_tile(&graph, ni);
+        // find which border will butt up against curr's EAST border (which is mirrored)
+        let mut border = tile.which_border(curr.1.get_flipped_border(EAST));
+        if border >= 4 {
+            // if the border is flipped, we need to flip the tile over.
+            tile.flip();
+            // and exchange EAST/WEST
+            if border % 2 == 1 {
+                border += 2;
+            }
         }
-        for e in t.edges() {
-            if let Some(n) = node_by_edge.insert(e, node) {
-                graph.update_edge(n, node, e);
-                graph.update_edge(node, n, e);
+        // rotate it so the target border is facing WEST
+        match border % 4 {
+            0 => tile.rotate(3),
+            1 => tile.rotate(2),
+            2 => tile.rotate(1),
+            3 => {}
+            _ => panic!("{} can't be which border?!", border),
+        }
+        // write it down!
+        curr = Rc::new((ni, tile));
+        grid.push(curr.clone());
+    }
+    drop(curr);
+    grid.into_iter()
+        .map(|rc| Rc::try_unwrap(rc).unwrap().1)
+        .collect::<Vec<_>>()
+}
+
+fn top_left_corner(graph: &DiGraph<&Tile, (usize, &String)>) -> (NodeIndex, Tile) {
+    let ni = find_corners(&graph)[0];
+    let mut tile = mungible_tile(graph, ni);
+    // flip/rotate it so that it's truly top-left
+    let directions = graph.edges(ni).map(|e| e.weight().0).collect::<Vec<_>>();
+    let mut min = directions[0] % 4;
+    let mut max = directions[1] % 4;
+    if max < min {
+        let t = min;
+        min = max;
+        max = t;
+    }
+    match min {
+        0 if max == 1 => tile.rotate(1),
+        0 if max == 3 => tile.rotate(2),
+        1 => {} // south east
+        2 => tile.rotate(3),
+        _ => panic!("{} can't be the min direction?!", min),
+    }
+
+    // allows visual sanity checking of the demo
+    tile.flip();
+    tile.rotate(3);
+    // println!("{}", tile);
+
+    (ni, tile)
+}
+
+fn mungible_tile(graph: &DiGraph<&Tile, (usize, &String)>, ni: NodeIndex) -> Tile {
+    let &gt = graph.node_weight(ni).unwrap();
+    Tile::new(gt.num, &gt.pixels)
+}
+
+fn find_corners(graph: &DiGraph<&Tile, (usize, &String)>) -> Vec<NodeIndex> {
+    graph
+        .node_indices()
+        .filter(|ni| graph.edges(*ni).count() == 2)
+        .collect()
+}
+
+fn build_graph(tiles: &[Tile]) -> DiGraph<&Tile, (usize, &String)> {
+    let mut node_by_edge = HashMap::new();
+    let mut graph = DiGraph::new();
+    for t in tiles {
+        let node = graph.add_node(t);
+        for (i, e) in t.all_borders().into_iter().enumerate() {
+            if let Some(existing) = node_by_edge.insert(e, node) {
+                if i >= 4 {
+                    // it's a flipped edge, which we needed to insert into the map,
+                    // but don't want in the graph.
+                    continue;
+                }
+                graph.add_edge(node, existing, (i, e));
+                let et = *graph.node_weight(existing).unwrap();
+                let ei = et.all_borders().iter().position(|oe| e == oe).unwrap();
+                graph.add_edge(existing, node, (ei, e));
             }
         }
     }
-    node_by_num
-        .iter()
-        .filter(|&(_, &ni)| graph.edges(ni).count() == 2)
-        .map(|(num, _)| num)
-        .product()
+    if cfg!(debug_assertions) {
+        let dim = sqrtusize(tiles.len());
+        debug_assert_eq!(tiles.len(), graph.node_count());
+        // The puzzle has dim-1 rows of dim overlapping vertical borders, and
+        // dim-1 columns of overlapping horizontal borders, each represented
+        // as two directed edges in the graph.
+        debug_assert_eq!(2 * 2 * dim * (dim - 1), graph.edge_count());
+    }
+    graph
 }
 
 #[cfg(test)]
@@ -238,17 +406,39 @@ Tile 3079:
     }
 
     #[test]
-    fn parse() {
+    fn test_parse() {
         let t = tile_2311();
         assert_eq!(2311, t.num);
-        assert_eq!("..##.#..#.", t.north);
-        assert_eq!("...#.##..#", t.east);
-        assert_eq!("..###..###", t.f_south);
-        assert_eq!(".#####..#.", t.f_west);
+        assert_eq!("..##.#..#.", t.get_border(NORTH));
+        assert_eq!("...#.##..#", t.get_border(EAST));
+        assert_eq!("###..###..", t.get_border(SOUTH));
+        assert_eq!(".#..#####.", t.get_border(WEST));
+    }
+
+    #[test]
+    fn test_flip() {
+        let mut t = tile_2311();
+        t.flip();
+        assert_eq!(".#..#.##..", t.get_border(NORTH));
+        assert_eq!(".#####..#.", t.get_border(EAST));
+        assert_eq!("..###..###", t.get_border(SOUTH));
+        assert_eq!("#..##.#...", t.get_border(WEST));
+    }
+
+    #[test]
+    fn test_rotate_cw() {
+        let mut t = tile_2311();
+        t.rotate(1);
+        assert_eq!(".#..#####.", t.get_border(NORTH));
+        assert_eq!("..##.#..#.", t.get_border(EAST));
+        assert_eq!("...#.##..#", t.get_border(SOUTH));
+        assert_eq!("###..###..", t.get_border(WEST));
     }
 
     #[test]
     fn example_one() {
-        assert_eq!(20899048083289, part_one(&EXAMPLE_ONE));
+        let tiles = parse(&EXAMPLE_ONE);
+        assert_eq!(20899048083289, part_one(&tiles));
+        assert_eq!(273, part_two(&tiles));
     }
 }
