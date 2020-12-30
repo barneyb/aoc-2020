c5cb5cacd98634be1bb8441d465910f4d035c418
Barney Boisvert <bboisvert@gmail.com>
2020-12-29 16:52:40 -0800

    lay in parse/unparse and tests from the examples

--- src/the_work.rs
+++ src/the_work.rs
@@ -1,177 +1,130 @@
-use aoc_2020::{read_input, time_block};
-use std::collections::{HashSet, VecDeque};
+use aoc_2020::time_block;
+use std::collections::VecDeque;
+use std::fmt;
+use std::fmt::{Debug, Display, Formatter};
 
 pub fn the_work() {
-    let game = time_block("Deal", || deal(&read_input()));
-    println!("{:?}", time_block("Part One", || part_one(game)));
-    let game = time_block("Deal Again", || deal(&read_input()));
-    println!("{:?}", time_block("Part Two", || part_two(game)));
+    let cups = time_block("Parsing", || Cups::from("469217538"));
+    println!("{:?}", time_block("Part One", || part_one(&cups)));
 }
 
-type Card = usize;
-type Deck = VecDeque<Card>;
-type Game = [Deck; 2];
-enum Player {
-    PlayerOne,
-    PlayerTwo,
+fn part_one(cups: &Cups) -> String {
+    cups.to_string()
 }
-use Player::*;
 
-fn part_one([mut one, mut two]: Game) -> usize {
-    loop {
-        // each flips their top card
-        let a = one.pop_front().unwrap();
-        let b = two.pop_front().unwrap();
-        // the player with the larger card gets both (theirs first)
-        if a > b {
-            one.push_back(a);
-            one.push_back(b);
-            if two.is_empty() {
-                // return the winner's score
-                return compute_score(&one);
-            }
-        } else {
-            two.push_back(b);
-            two.push_back(a);
-            if one.is_empty() {
-                // return the winner's score
-                return compute_score(&two);
-            }
-        }
+#[derive(Eq, PartialEq)]
+struct Cups {
+    moves: usize,
+    ring: VecDeque<usize>,
+}
+
+impl Cups {
+    fn tick(&mut self) {
+        self.moves += 1;
+        // todo: do some things!
     }
 }
 
-fn part_two(mut game: Game) -> usize {
-    let winner = play_recursive(&mut game);
-    compute_score(&game[winner as usize])
+impl From<&str> for Cups {
+    fn from(s: &str) -> Self {
+        Cups::from(s.parse::<usize>().unwrap())
+    }
 }
 
-fn play_recursive([one, two]: &mut Game) -> Player {
-    let mut seen_states = HashSet::new();
-    loop {
-        // if it's a repeated state player one wins the game
-        if !seen_states.insert((
-            one.iter().cloned().collect::<Vec<_>>(),
-            two.iter().cloned().collect::<Vec<_>>(),
-        )) {
-            return PlayerOne;
-        }
-        // each flips their top card
-        let a = one.pop_front().unwrap();
-        let b = two.pop_front().unwrap();
-        let round_winner = if one.len() >= a && two.len() >= b {
-            // make new decks of first n cards of each deck
-            // play a recursive game; winner wins the round
-            let mut subgame = [
-                one.iter().take(a).cloned().collect::<Deck>(),
-                two.iter().take(b).cloned().collect::<Deck>(),
-            ];
-            play_recursive(&mut subgame)
-        } else if a > b {
-            PlayerOne
-        } else {
-            PlayerTwo
+/*
+ * Parsing shift numbers down by one, so that we can use modulo arithmetic
+ * easily. Rendering shifts numbers up by one so they rematch the problem.
+ */
+
+impl From<usize> for Cups {
+    fn from(mut n: usize) -> Self {
+        let mut cups = Cups {
+            moves: 0,
+            ring: VecDeque::new(),
         };
-        // the round's winner gets both cards (theirs first)
-        match round_winner {
-            PlayerOne => {
-                one.push_back(a);
-                one.push_back(b);
-                if two.is_empty() {
-                    return PlayerOne;
-                }
-            }
-            PlayerTwo => {
-                two.push_back(b);
-                two.push_back(a);
-                if one.is_empty() {
-                    return PlayerTwo;
-                }
-            }
+        while n > 0 {
+            cups.ring.push_front(n % 10 - 1);
+            n /= 10;
         }
+        cups
     }
 }
 
-fn compute_score(deck: &Deck) -> usize {
-    deck.iter()
-        .fold((0, deck.len()), |(score, offset), card| {
-            (score + card * offset, offset - 1)
-        })
-        .0
+impl Display for Cups {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let mut buffer = self.ring.clone();
+        loop {
+            if let Some(0) = buffer.front() {
+                break;
+            }
+            buffer.rotate_left(1);
+        }
+        for d in buffer.iter().skip(1) {
+            write!(f, "{}", d + 1)?;
+        }
+        Ok(())
+    }
 }
 
-fn deal(input: &str) -> Game {
-    let mut players = input.trim().split("\n\n").map(|player| {
-        player
-            .trim()
-            .lines()
-            .skip(1)
-            .map(|c| c.parse::<Card>().unwrap())
-            .collect()
-    });
-    let game = [players.next().unwrap(), players.next().unwrap()];
-    debug_assert!(players.next().is_none());
-    game
+impl Debug for Cups {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let curr = self.moves % self.ring.len();
+        for (i, n) in self.ring.iter().enumerate() {
+            if i == curr {
+                write!(f, "({})", n + 1)?;
+            } else {
+                write!(f, " {} ", n + 1)?;
+            }
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const EXAMPLE_ONE: &str = "\
-Player 1:
-9
-2
-6
-3
-1
+    #[test]
+    fn test_parse_and_unparse() {
+        let cups = Cups::from("3412");
+        assert_eq!(0, cups.moves);
+        assert_eq!(VecDeque::from(vec![2usize, 3, 0, 1]), cups.ring);
+        assert_eq!("(3) 4  1  2 ", format!("{:?}", cups));
+        assert_eq!("234", cups.to_string());
+    }
 
-Player 2:
-5
-8
-4
-7
-10";
+    const EXAMPLE_ONE: &str = "389125467";
 
     #[test]
-    fn test_parse() {
-        let [a, b] = deal(&EXAMPLE_ONE);
-        assert_eq!(vec![9, 2, 6, 3, 1], a.into_iter().collect::<Vec<_>>());
-        assert_eq!(vec![5, 8, 4, 7, 10], b.into_iter().collect::<Vec<_>>());
+    fn test_single_moves() {
+        let mut cups = Cups::from(EXAMPLE_ONE);
+        assert_eq!("(3) 8  9  1  2  5  4  6  7 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 3 (2) 8  9  1  5  4  6  7 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 3  2 (5) 4  6  7  8  9  1 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 7  2  5 (8) 9  1  3  4  6 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 3  2  5  8 (4) 6  7  9  1 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 9  2  5  8  4 (1) 3  6  7 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 7  2  5  8  4  1 (9) 3  6 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 8  3  6  7  4  1  9 (2) 5 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 7  4  1  5  8  3  9  2 (6)", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!("(5) 7  4  1  8  3  9  2  6 ", format!("{:?}", cups));
+        cups.tick();
+        assert_eq!(" 5 (8) 3  7  4  1  9  2  6 ", format!("{:?}", cups));
+        assert_eq!("92658374", cups.to_string())
     }
 
     #[test]
-    fn test_compute_score() {
-        let deck: Deck = vec![3, 2, 10, 6, 8, 5, 9, 4, 7, 1].into_iter().collect();
-        assert_eq!(306, compute_score(&deck));
-    }
-
-    #[test]
-    fn example_one_part_one() {
-        let game = deal(&EXAMPLE_ONE);
-        assert_eq!(306, part_one(game));
-    }
-
-    const EXAMPLE_TWO: &str = "\
-Player 1:
-43
-19
-
-Player 2:
-2
-29
-14";
-
-    #[test]
-    fn example_two() {
-        let game = deal(&EXAMPLE_TWO);
-        let score = compute_score(&game[PlayerOne as usize]);
-        assert_eq!(score, part_two(game));
-    }
-
-    #[test]
-    fn example_one_part_two() {
-        let game = deal(&EXAMPLE_ONE);
-        assert_eq!(291, part_two(game));
+    fn example_one() {
+        let ring = Cups::from(EXAMPLE_ONE);
+        assert_eq!("67384529", part_one(&ring));
     }
 }
